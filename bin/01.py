16ded05bbf2b175ec0bd2b6a2f9b27ef7cf22e66
pschwede <pschwede@posteo.de>
2020-12-02 00:03:17 +0100

    refactoring

--- bin/01.py
+++ bin/01.py
@@ -5,18 +5,16 @@ from typing import List
 """Find two summands """
 def summands(sum_: int, xs: List[int], n: int=1) -> int:
     if n==1:
-        if sum_ in xs:
-            return [sum_]
-        return None
-    for i, a in enumerate(xs):
+        return [sum_] if sum_ in xs else None
+    for i, x in enumerate(xs):
         if n>0:
-            res = summands(sum_ - a, xs[i+1:], n-1)
+            res = summands(sum_ - x, xs[i+1:], n-1)
             if res:
-                return [a] + res
+                return [x] + res
 
 if __name__ == "__main__":
     with open('res/01.txt', 'r') as f:
-        for n in range(1,6):
+        for n in range(1,4):
             f.seek(0)
             smmds = summands(2020, [int(line) for line in f], n=n)
             if smmds:
