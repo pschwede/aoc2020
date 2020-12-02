66c7459b4c2fff079658ac7e15a4d0cb5bdc2137
pschwede <pschwede@posteo.de>
2020-12-02 14:15:45 +0100

    reorganized

---
+++ bin/01.01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from functools import reduce
+
+from lib.summands import summands
+
+if __name__ == "__main__":
+    with open('res/01.txt', 'r') as f:
+        f.seek(0)
+        res = summands(2020, [int(line) for line in f], n=2)
+        if res:
+            print(reduce(int.__mul__, res, 1))
